6160ec57f5c5e195340ae1629e5e4a9d25826e06
Jesús De la Fuente <jesusdelafureyesjdlfr@gmail.com>
2023-03-03 22:34:52 -0600

    Inicio

---
+++ Prueba1/src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AngularFireModule } from '@angular/fire/compat'
+import { AngularFireAuthModule } from '@angular/fire/compat/auth'
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore'
+import { environment } from 'src/environments/environment';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { HomeComponent } from './components/home/home.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAuthModule,
+    AngularFirestoreModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ Prueba1/src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    production: false,
+    firebase: {
+        apiKey: "AIzaSyDs52JUYxntPeO-mqfwvZaJmJQbbE35j3k",
+        authDomain: "angularfirebase-931a4.firebaseapp.com",
+        projectId: "angularfirebase-931a4",
+        storageBucket: "angularfirebase-931a4.appspot.com",
+        messagingSenderId: "595821626523",
+        appId: "1:595821626523:web:3a1be7a93fea83943a723e"
+      }  
+};
